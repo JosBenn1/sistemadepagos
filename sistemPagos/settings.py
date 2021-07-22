6c9753ffa28fe249aa2e036453f690009443b08c
JosBenn1 <66485386+JosBenn1@users.noreply.github.com>
2021-07-22 14:36:30 -0500

    Update settings.py

--- sistemPagos/settings.py
+++ sistemPagos/settings.py
@@ -133,8 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core/static'),
 ]
